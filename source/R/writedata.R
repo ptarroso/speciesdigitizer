c9621d8bf8b4ff3586a0e2f7a5c05cc5f1570e06
Pedro Tarroso <ptarroso@cibio.up.pt>
2016-05-25 11:39:03 +0100

    Forced file extesion to be ".txt" when saving the results

--- source/R/writedata.R
+++ source/R/writedata.R
@@ -2,13 +2,14 @@ writedata <-
 function()
 ### Writes species data to file
 {
-    fileName <- tclvalue(tkgetSaveFile())
+    fileName <- tclvalue(tkgetSaveFile(filetypes = "{text {.txt}}"))
     if (!nchar(fileName)) {
         tkmessageBox(message = "No file was selected!")
     } else {
         write.table(spDigit$mapSpPoints, fileName, row.names=FALSE,
                     col.names = TRUE, sep = ';', quote = FALSE)
 
+        ## Save georeferenced map
         if (tclvalue(spDigit$savemap) == "1") {
             ## Copy map image with world file.
             baseName <- strsplit(fileName, "\\.")[[1]]
