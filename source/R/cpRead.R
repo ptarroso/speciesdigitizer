8d70ad7a64529f1a245936c923c911171dedb330
ptarroso <ptarroso@cibio.up.pt>
2020-11-19 23:44:51 +0000

    Control points can have more than 2 colums (but are ignored)

--- source/R/cpRead.R
+++ source/R/cpRead.R
@@ -3,7 +3,7 @@ function(cpfile)
 ### Reads the contents of a control points file
 {
     cp <- read.table(cpfile, sep = ";", header = TRUE)
-    if (ncol(cp) != 2) {
+    if (ncol(cp) < 2) {
         tkmessageBox(message = paste("The control points file must have at",
                                      "least 2 columns with X and Y.\nCheck the",
                                      "if the separator is \";\" and if the",
